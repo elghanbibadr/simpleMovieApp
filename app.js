b015abb336d1ec0319d7d69c643bfddc99690415
badr ghanbiE <bghaE@gmail.com>
2022-10-11 14:26:20 +0100

    add the input functionalitty

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5
 
 // variables
 let main=document.querySelector('main');
+let form=document.querySelector('form');
 
 // when the page first entered the 1 page movie will be automaticlly displayd
 function getFirstPageMovies(){
@@ -11,7 +12,7 @@ function getFirstPageMovies(){
      fetch(apiUrl)
      .then(response=>response.json())
      .then(firstPageData=>{
-        console.log(firstPageData)
+        // console.log(firstPageData)
         // add the movies of the first page to the Dom
         firstPageData.results.forEach((result)=>{
             addMovieToThePageFrom(result)
@@ -46,3 +47,48 @@ function addMovieToThePageFrom(data){
 
 
 
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let SearchTerm=form.firstElementChild.value;
+    // check if the input not empty and contains valid string
+    let regex=/^[A-Za-z0-9]*$/;
+    if (!SearchTerm || !regex.test(SearchTerm)) {
+        alert('Please enter a valid movie name')
+        return;
+    }
+     fetch(SEARCHAPI + SearchTerm)
+     .then(res=>res.json())
+     .then(data=>{
+
+        // lets first check if we got some valid results or not
+         let searchedMovies=data.results;
+         if (searchedMovies.length===0){
+             alert('opps there is no movie with this name ')
+             return;
+         }
+
+        // lets now update the dom according to results
+          main.innerHTML='';
+        //   filter results by movies that has an image
+          let searchedMoviesThatHasImages=searchedMovies.filter(element=>element.poster_path)
+          
+          searchedMoviesThatHasImages.forEach(element=>{
+          addMovieToThePageFrom(element);
+         })
+
+     })
+   
+    
+})
+
+
+// fetch("https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=saw")
+// .then(res=>res.json())
+// .then(data=>console.log(data))
+
+form.querySelector('input').addEventListener('input',function(){
+    if(this.value.length===0){
+        main.innerHTML='';
+        getFirstPageMovies();
+    }
+})
